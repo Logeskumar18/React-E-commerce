8185e7c5a03e18bf5de4116bed7651a15f48ca16
Loges <kumarloges329@gmail.com>
2025-05-20 07:26:57 +0530

    Commited

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,21 +4,44 @@ import { FaCartPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav className="d-flex justify-content-between align-items-center bg-light shadow-lg">
-      <h1 className="w-50 fs-3 ps-5">
-        <IoBag />
-        L-Mart
-      </h1>
-      <ul className="w-50 d-flex justify-content-between pe-5 pt-3">
-        <Link className="text-decoration-none text-black fs-5 fw-semibold " to="/">Home</Link>
-        <Link className="text-decoration-none text-black fs-5 fw-semibold " to="/shop">Shop</Link>
-        <Link className="text-decoration-none text-black fs-5 fw-semibold " to="/cart">Cart</Link>
-        <Link className="text-decoration-none text-black fs-5 fw-semibold " to="/profile"><FaUser /></Link>
-        <Link className="text-decoration-none text-black fs-5 fw-semibold " to="/cart"><FaCartPlus /></Link>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg bg-light shadow-lg">
+            <div className="container-fluid">
+                <Link className="navbar-brand fs-3 fw-bold d-flex align-items-center" to="/">
+                    <IoBag className="me-2" />
+                    L-Mart
+                </Link>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto d-flex align-items-center gap-4 pe-3">
+                        <li className="nav-item">
+                            <Link className="nav-link fw-semibold fs-5 text-black" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link fw-semibold fs-5 text-black" to="/shop">Shop</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link fw-semibold fs-5 text-black" to="/cart">Cart</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-black fs-5" to="/profile">
+                                <FaUser />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-black fs-5" to="/cart">
+                                <FaCartPlus />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
-export default Navbar
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createRoot } from 'react-dom/client';
 import Home from './Pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 
 createRoot(document.getElementById('root')).render(
   <>
+   
   <BrowserRouter>
+  <Navbar />
     <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes>
